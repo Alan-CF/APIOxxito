6c5e421d1118b33f83f08edae62a875e5b428cfc
Alan Canales <alanyaco.cf@gmail.com>
2025-05-22 18:41:35 -0600

    [REFACTOR] Versus rename

--- Controllers/VersusController.cs
+++ Controllers/VersusController.cs
@@ -6,7 +6,7 @@ namespace APIOxxito.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class JuegoController : ControllerBase
+public class VersusController : ControllerBase
 {
   public string ConnectionString = "Server=mysql-373b7fe1-danielara071-6268.g.aivencloud.com;Port=24232;Database=mi_oxxito;Uid=avnadmin;Pwd=AVNS_ZJOL4SKtMmgE-f7N-_W;SslMode=none;";
 
