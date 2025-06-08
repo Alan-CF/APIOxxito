849adf23f1b4a556ba46cb36626cdb2e8de89190
a01199013 <150080668+a01199013@users.noreply.github.com>
2025-06-08 14:10:27 -0600

    leadeboard

---
+++ PerfilRanking.cs
@@ -0,0 +1,10 @@
+namespace APIOxxito;
+
+public class PerfilRanking
+{
+    public string? Nombre { get; set; }
+    public string? Sede { get; set; }
+    public int PuntosTotales { get; set; }
+    public int Ranking { get; set; }
+    public int asesor { get; set; }
+}
---
+++ UserRank.cs
@@ -0,0 +1,7 @@
+namespace APIOxxito;
+
+public class UserRank
+{
+  public int userRank { get; set; }
+  public int puntosTotales { get; set; }
+}
