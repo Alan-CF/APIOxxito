fb7aab4fd83d87624e1f555d8d234435c5234b10
German-coder07 <gercuetol@gmail.com>
2025-06-09 14:33:10 -0600

    [ADD] funcion para que funcione la api en web gl

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
+builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 // Forzar redirección a HTTPS
 builder.Services.AddHttpsRedirection(options =>
@@ -25,6 +35,7 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
